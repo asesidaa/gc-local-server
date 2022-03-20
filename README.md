4c42a9376ff1f399bb2a470e87a283d91836360b
Yuchen Ji <jiych1@shanghaitech.edu.cn>
2022-03-21 00:00:55 +0800

    Update README, add backup files

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 A very much incomplete GC4EX server emulator
 
 Based on https://github.com/fatal-bundy/nesys_at_home
+
+# Usage
+
+1. First patch NesysService.exe, point all url to localhost
+2. Generate a self-signed root CA, **with common name=Taito Arcade Machine CA**
+3. Use the generated root CA to sign a certificate for localhost
+4. Import the generated root CA (**with private key!!**) to Personal and trusted root CA tab
+5. On localhost, set up a webserver with whatever program, enable https
+6. Create a database, you can change the database config in config/config.inc
+7. Create 6 tables, schemas dumped in 2110_card.xml using phpmyadmin
+8. For music_unlock and music_extra, backup data are exported to .sql files
+7. For card_main, first import the backup data then for card with id 7020392010281502, change the card id and player
+   name of your choice
+7. Open the game, without nesys emu, you should be able to use the card
+
+# Missing functions
+
+- [ ] No saving for avatar/navigator/title/se/skin selection
+- [ ] Coin is fixed at 999999
+- [ ] Reward unlocking is broken, wait 60s or the game will crash
---
+++ card_main.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1
+-- 生成日期： 2022-03-20 16:41:19
+-- 服务器版本： 10.4.22-MariaDB
+-- PHP 版本： 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `2110_card`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `card_main`
+--
+
+CREATE TABLE `card_main`
+(
+    `card_id`        varchar(16) NOT NULL,
+    `player_name`    text        NOT NULL,
+    `score_i1`       int(11)     NOT NULL,
+    `fcol1`          int(11)     NOT NULL,
+    `fcol2`          int(11)     NOT NULL,
+    `fcol3`          int(11)     NOT NULL,
+    `achieve_status` text        NOT NULL,
+    `created`        int(11)     NOT NULL DEFAULT 0,
+    `updated`        int(11)     NOT NULL DEFAULT 0
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+--
+-- 转存表中的数据 `card_main`
+--
+
+INSERT INTO `card_main` (`card_id`, `player_name`, `score_i1`, `fcol1`, `fcol2`, `fcol3`, `achieve_status`, `created`,
+                         `updated`)
+VALUES ('7020392000147361', 'test2', 0, 0, 0, 45200, '0', 0, 0),
+       ('7020392010281502', 'test0', 0, 0, 0, 45200, '0', 1, 0);
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `card_main`
+--
+ALTER TABLE `card_main`
+    ADD UNIQUE KEY `card_id` (`card_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
